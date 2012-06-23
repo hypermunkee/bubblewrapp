ec6fbd662ca4b4613169ecd7e7d7272881176252
Andrew Wong <andrewchhwong@gmail.com>
2012-06-23 15:36:01 -0700

    Added db connection code

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 import os
 
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://username:password@http://bubblewrapp.herokuapp.com/shared-database'
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
