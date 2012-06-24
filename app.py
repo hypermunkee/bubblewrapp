6dea5919461c6749ee55c6e7971e67562287b90e
Andrew Wong <andrewchhwong@gmail.com>
2012-06-23 23:00:23 -0700

    forcing garbage down throat

--- app.py
+++ app.py
@@ -1,107 +1,61 @@
-# -*- coding: utf-8 -*-
-"""
-Flask-Login example
-===================
-This is a small application that provides a trivial demonstration of
-Flask-Login, including remember me functionality.
-
-:copyright: (C) 2011 by Matthew Frazier.
-:license:   MIT/X11, see LICENSE for more details.
-"""
 import os
 
-from flask import Flask, request, render_template, redirect, url_for, flash
-from flask.ext.login import (LoginManager, current_user, login_required,
-                            login_user, logout_user, UserMixin, AnonymousUser,
-                            confirm_login, fresh_login_required)
-
-class User(UserMixin):
-    def __init__(self, name, id, active=True):
-        self.name = name
-        self.id = id
-        self.active = active
-    
-    def is_active(self):
-        return self.active
+from flask import Flask, redirect, url_for, session, request
+from flaskext.oauth import OAuth
 
 
-class Anonymous(AnonymousUser):
-    name = u"Anonymous"
-
-
-USERS = {
-    1: User(u"Notch", 1),
-    2: User(u"Steve", 2),
-    3: User(u"Creeper", 3, False),
-}
-
-USER_NAMES = dict((u.name, u) for u in USERS.itervalues())
+SECRET_KEY = 'development key'
+DEBUG = True
+FACEBOOK_APP_ID = '148264965297954'
+FACEBOOK_APP_SECRET = '4a82e7ae7863152a973e68d9d483064a'
 
 
 app = Flask(__name__)
+app.debug = DEBUG
+app.secret_key = SECRET_KEY
+oauth = OAuth()
 
-SECRET_KEY = "yeah, not actually a secret"
-DEBUG = True
-
-app.config.from_object(__name__)
-
-login_manager = LoginManager()
-
-login_manager.anonymous_user = Anonymous
-login_manager.login_view = "login"
-login_manager.login_message = u"Please log in to access this page."
-login_manager.refresh_view = "reauth"
-
-@login_manager.user_loader
-def load_user(id):
-    return USERS.get(int(id))
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': 'email'}
+)
 
 
-login_manager.setup_app(app)
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return redirect(url_for('login'))
 
 
-@app.route("/secret")
-@fresh_login_required
-def secret():
-    return render_template("secret.html")
-
-
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login')
 def login():
-    if request.method == "POST" and "username" in request.form:
-        username = request.form["username"]
-        if username in USER_NAMES:
-            remember = request.form.get("remember", "no") == "yes"
-            if login_user(USER_NAMES[username], remember=remember):
-                flash("Logged in!")
-                return redirect(request.args.get("next") or url_for("index"))
-            else:
-                flash("Sorry, but you could not log in.")
-        else:
-            flash(u"Invalid username.")
-    return render_template("login.html")
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
 
 
-@app.route("/reauth", methods=["GET", "POST"])
-@login_required
-def reauth():
-    if request.method == "POST":
-        confirm_login()
-        flash(u"Reauthenticated.")
-        return redirect(request.args.get("next") or url_for("index"))
-    return render_template("reauth.html")
+@app.route('/login/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['oauth_token'] = (resp['access_token'], '')
+    me = facebook.get('/me')
+    return 'Logged in as id=%s name=%s redirect=%s' % \
+        (me.data['id'], me.data['name'], request.args.get('next'))
 
 
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    flash("Logged out.")
-    return redirect(url_for("index"))
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('oauth_token')
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
