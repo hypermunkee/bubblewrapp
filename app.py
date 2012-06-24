71a9f7bef2969a1e32a5fc49a4229e3a5fab508c
Andrew Wong <andrewchhwong@gmail.com>
2012-06-23 18:18:40 -0700

    Fixed flask-login import

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ Flask-Login, including remember me functionality.
 :license:   MIT/X11, see LICENSE for more details.
 """
 from flask import Flask, request, render_template, redirect, url_for, flash
-from flaskext.login import (LoginManager, current_user, login_required,
+from flask.ext.login import (LoginManager, current_user, login_required,
                             login_user, logout_user, UserMixin, AnonymousUser,
                             confirm_login, fresh_login_required)
 
