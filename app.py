8569cefdd109338310584a8b37b1e11ae8c1b88c
Andrew Wong <andrewchhwong@gmail.com>
2012-06-24 13:19:46 -0700

    Added missing relation between users and bundles

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ db = SQLAlchemy(app)
 class Bundle(db.Model):
 	__tablename__ = 'bundle'
 	id = db.Column(db.Integer, primary_key=True)
+	owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 	title = db.String(150)
 	description = db.Column(db.Text)
 	hash = db.Column(db.String(60))
@@ -77,6 +78,7 @@ class MediaType:
 
 
 class User(db.Model):
+	__tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
