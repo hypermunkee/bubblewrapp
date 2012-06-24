7b6672606d1e2b7c89554ca17712d12b55b55ac5
Andrew Wong <andrewchhwong@gmail.com>
2012-06-23 18:00:01 -0700

    flask-login test

--- app.py
+++ app.py
@@ -1,34 +1,105 @@
-import os
+# -*- coding: utf-8 -*-
+"""
+Flask-Login example
+===================
+This is a small application that provides a trivial demonstration of
+Flask-Login, including remember me functionality.
 
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask import render_template
+:copyright: (C) 2011 by Matthew Frazier.
+:license:   MIT/X11, see LICENSE for more details.
+"""
+from flask import Flask, request, render_template, redirect, url_for, flash
+from flaskext.login import (LoginManager, current_user, login_required,
+                            login_user, logout_user, UserMixin, AnonymousUser,
+                            confirm_login, fresh_login_required)
+
+class User(UserMixin):
+    def __init__(self, name, id, active=True):
+        self.name = name
+        self.id = id
+        self.active = active
+    
+    def is_active(self):
+        return self.active
+
+
+class Anonymous(AnonymousUser):
+    name = u"Anonymous"
+
+
+USERS = {
+    1: User(u"Notch", 1),
+    2: User(u"Steve", 2),
+    3: User(u"Creeper", 3, False),
+}
+
+USER_NAMES = dict((u.name, u) for u in USERS.itervalues())
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qdvcwjhjzx:KzsY5P8JzVuCY60RDmQ1@ec2-107-20-152-105.compute-1.amazonaws.com/qdvcwjhjzx'
-db = SQLAlchemy(app)
+
+SECRET_KEY = "yeah, not actually a secret"
+DEBUG = True
+
+app.config.from_object(__name__)
+
+login_manager = LoginManager()
+
+login_manager.anonymous_user = Anonymous
+login_manager.login_view = "login"
+login_manager.login_message = u"Please log in to access this page."
+login_manager.refresh_view = "reauth"
+
+@login_manager.user_loader
+def load_user(id):
+    return USERS.get(int(id))
 
 
-@app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+login_manager.setup_app(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+@app.route("/secret")
+@fresh_login_required
+def secret():
+    return render_template("secret.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST" and "username" in request.form:
+        username = request.form["username"]
+        if username in USER_NAMES:
+            remember = request.form.get("remember", "no") == "yes"
+            if login_user(USER_NAMES[username], remember=remember):
+                flash("Logged in!")
+                return redirect(request.args.get("next") or url_for("index"))
+            else:
+                flash("Sorry, but you could not log in.")
+        else:
+            flash(u"Invalid username.")
+    return render_template("login.html")
+
+
+@app.route("/reauth", methods=["GET", "POST"])
+@login_required
+def reauth():
+    if request.method == "POST":
+        confirm_login()
+        flash(u"Reauthenticated.")
+        return redirect(request.args.get("next") or url_for("index"))
+    return render_template("reauth.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Logged out.")
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run()
