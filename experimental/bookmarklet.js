37832d192ec2fa3837aeba929e9103ba026e17b5
Andrew Wong <andrewchhwong@gmail.com>
2012-06-24 18:49:15 -0700

    Oops committed duplicate code in previous bookmarklet CL

--- experimental/bookmarklet.js
+++ experimental/bookmarklet.js
@@ -1,6 +1,6 @@
 javascript:void((function() {
 	var grabYouTubeId = function() {
-		// If on a YouTube page, this will grab the YouTube ID.
+		/* If on a YouTube page, this will grab the YouTube ID. */
 		var url = location.href.replace(/https?:\/\//i, "");
 		var pattern = /(http:\/\/)?(?:www\.)?youtube.com\/watch\?(?=.*v=[\-\w]+)(?:\S+)?$/;
 		var id;
@@ -14,27 +14,7 @@ javascript:void((function() {
 		var ytId = grabYouTubeId(); alert("Found ID: " + ytId);
 	};
 
-	if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
-		script = document.createElement('script');
-		script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
-		script.onload=displayModalWindow;
-		document.body.appendChild(script);
-	} else {
-		displayModalWindow();
-	}
-})());
-
-
-javascript:void((function() {
-	var grabYouTubeId = function() {
-		return 99;
-	};
-
-	var displayModalWindow = function() {
-		var ytId = grabYouTubeId(); alert("Found ID: " + ytId);
-	};
-
-	if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
+	if (!($ = window.jQuery)) { /* typeof jQuery=='undefined' works too */
 		script = document.createElement('script');
 		script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
 		script.onload=displayModalWindow;
