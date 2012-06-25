f5f2ac593a18a50dda418409b0064b97912b5075
Andrew Wong <andrewchhwong@gmail.com>
2012-06-24 20:16:49 -0700

    Fleshed out bookmarklet js to pull all YouTube video titles & IDs

--- experimental/bookmarklet.js
+++ experimental/bookmarklet.js
@@ -1,17 +1,41 @@
 javascript:void((function() {
-	var grabYouTubeId = function() {
-		/* If on a YouTube page, this will grab the YouTube ID. */
-		var url = location.href.replace(/https?:\/\//i, "");
+	var grabYouTubeIds = function() {
+		var ids = new Array();
+
+		/* If on a YouTube page, this will grab the primary YouTube ID. */
+		var url = location.href.replace(/https?:\/\//i, '');
 		var pattern = /(http:\/\/)?(?:www\.)?youtube.com\/watch\?(?=.*v=[\-\w]+)(?:\S+)?$/;
-		var id;
 		if (pattern.test(url)) {
-			id = $('input[name="video_id"]').val();
+			var title = $('#eow-title').attr('title');
+			var videoId = $('input[name="video_id"]').val();
+			ids.push([title, videoId]);
 		}
-		return id;
+
+		/* Grab all related video IDs. */
+		$('li.video-list-item > a.related-video').each(function(index, element) {
+			var title = '';
+			var children = element.childNodes;
+			for (var i = 0; i < children.length; i++) {
+				var child = children[i];
+				if (child.className == 'title') {
+					title = child.title;
+					break;
+				}
+			}
+			var videoId = element.href.replace(/^[^v]+v.(.{11}).*/,"$1");
+			ids.push([title, videoId]);
+		});
+
+		return ids;
 	};
 
 	var displayModalWindow = function() {
-		var ytId = grabYouTubeId(); alert("Found ID: " + ytId);
+		var ytIds = grabYouTubeIds();
+		var dispStr = 'Found IDs:\n';
+		$.each(ytIds, function(index, element) {
+			dispStr += element[0] + ': ' + element[1] + '\n';
+		});
+		alert(dispStr);
 	};
 
 	if (!($ = window.jQuery)) { /* typeof jQuery=='undefined' works too */
