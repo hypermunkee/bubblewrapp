b205c5b5677b32810b57befa11e33718a5cbb619
Andrew Boni <andrewfboni@gmail.com>
2012-06-30 21:03:32 -0700

    Laid out the basic UI for the bookmarklet modal window. Still need to grab the viewcounts of all the different videos on the page.

--- experimental/bookmarklet.js
+++ experimental/bookmarklet.js
@@ -8,7 +8,8 @@ javascript:void((function() {
 		if (pattern.test(url)) {
 			var title = $('#eow-title').attr('title');
 			var videoId = $('input[name="video_id"]').val();
-			ids.push([title, videoId]);
+                        var views = parseInt($(".watch-view-count strong").html().replace(/\D/g,''));
+			ids.push([title, videoId, views]);
 		}
 
 		/* Grab all related video IDs. */
@@ -21,29 +22,61 @@ javascript:void((function() {
 					title = child.title;
 					break;
 				}
+                                
 			}
 			var videoId = element.href.replace(/^[^v]+v.(.{11}).*/,"$1");
-			ids.push([title, videoId]);
+			ids.push([title, videoId, views]);
 		});
 
 		return ids;
 	};
 
 	var displayModalWindow = function() {
-		var ytIds = grabYouTubeIds();
-		var dispStr = 'Found IDs:\n';
+                /* Clear the modal window body */
+                $('.modal-body').empty();
+		var ytIds = $.unique(grabYouTubeIds());
+                alert(ytIds);
 		$.each(ytIds, function(index, element) {
-			dispStr += element[0] + ': ' + element[1] + '\n';
-		});
-		alert(dispStr);
+                        /* Generate a random number between 200 and 400 */
+                        var randomNum = Math.ceil(Math.random()*200)+200;
+                        $(".modal-body").append("<p class='bubblewrapp-image' style='width:"+randomNum+"px;margin:10px;float:left;'>"+
+                        "<img style='float:left;box-shadow: #000 0 2px 10px;' src='http://img.youtube.com/vi/"+element[1]
+                        +"/0.jpg' /><span class='bubblewrapp-image-title' style='float:left;color:#fff;"+
+                        "margin-top:-40px;background-color:rgba(0,0,0,0.7);padding:0 5px;' >"+element[0]+"</span></p>");
+                });
+                
+                /* Wait until images are loaded before we apply masonry to them */
+                var $container = $('.modal-body');
+                $container.imagesLoaded(function(){
+                  $container.masonry({
+                    itemSelector : '.bubblewrapp-image'
+                  });
+                });
+                
+                /* Show the modal window */
+                $('#myModal').modal('show');
 	};
 
+        
+        
 	if (!($ = window.jQuery)) { /* typeof jQuery=='undefined' works too */
 		script = document.createElement('script');
 		script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
 		script.onload=displayModalWindow;
 		document.body.appendChild(script);
 	} else {
-		displayModalWindow();
+                /* Load Twitter bootstrap dependencies & masonry */
+                $('head').append('<link rel="stylesheet" href="http://www.bubblewrapp.com/static/bootstrap.css" type="text/css" />');
+                $('head').append('<script type="text/javascript" src="http://www.bubblewrapp.com/static/js/jquery.masonry.min.js" />');
+                $('head').append('<script type="text/javascript" src="http://www.bubblewrapp.com/static/bootstrap.js" />');
+                
+                $('body').append('<div style="width:900px !important; margin:-425px 0 0 -450px !important; max-height:1000px !important;" class="modal hide" id="myModal">'+
+                    '<div class="modal-header"><button type="button" class="close" data-dismiss="modal">×</button>'+
+                    '<img src="http://bubblewrapp.herokuapp.com/static/images/logo-small-black.png"'+
+                    ' alt="Bubblewrapp logo" /></div><div class="modal-body"><p>Choose a video to add</p></div>'+
+                    '<div class="modal-footer"><a href="#" class="btn" data-dismiss="modal">Close</a>'+
+                    '<a href="http://bubblewrapp.herokuapp.com/createbundle?params=put+things+here" class="btn btn-primary">Add to bundle</a></div></div>');
+                displayModalWindow();
+
 	}
 })());
